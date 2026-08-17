7f0de03dff9438d6ec10383791ac14a8325ef574
unknown <sarthak.agrawal1311@gmail.com>
2026-08-17 12:30:29 +0530

    removed db routes, pending future requirements

--- backend/app/api/v1/db.py
+++ backend/app/api/v1/db.py
@@ -5,24 +5,4 @@ from app.core.db import init_db
 router = APIRouter()
 
 
-@router.get("/reset-db")
-async def reset_db():
-    try:
-        init_db(reset=True)
-
-        return {
-            "status": "success",
-            "database": "postgres",
-            "message": "reset successful!",
-        }
-
-    except Exception as e:
-        print(f"Database reset failed! : {e}")
-
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="databse reset failed!",
-        )
-
-
 
